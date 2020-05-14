e62aef22fa75e84d1ab709c6bd12e1288f77e806
Chris <sears.topher@gmail.com>
2020-05-14 16:00:37 -0400

    cms preview

--- src/cms/preview-templates/NewsPostPreview.js
+++ src/cms/preview-templates/NewsPostPreview.js
@@ -1,38 +1,20 @@
 import React from 'react'
-// import PropTypes from 'prop-types'
-//import { NewsItem } from '../components/newsItem'
 
 import "../../styles/layout.css"
+import "../../styles/cmsPreviewStyle.css"
 
 const NewsPostPreview = ({ entry, widgetFor }) => {
-//   const tags = entry.getIn(['data', 'tags'])
   return (
-    <ol>
-        <li className="news-item">
-        <span className="news-item-header">
-            <h1 className="news-item-headline">{entry.getIn(['data', 'title'])}</h1>
-            <p className="news-post-date">{entry.getIn(['data', 'date'])}</p>
-        </span>
-        <div
-            className="news-item-html"
-            dangerouslySetInnerHTML={{ __html: widgetFor('body') }}
-        />
-        </li>
-    </ol>
-
-    // <NewsItem
-    //   html={widgetFor('body')}
-    //   title={entry.getIn(['data', 'title'])}
-    //   date={entry.getIn(['data', 'date'])}
-    // />
+      <ol>
+          <li className="news-item">
+          <span className="news-item-header">
+             <h1 className="news-item-headline">{entry.getIn(['data', 'title'])}</h1>
+             <p className="news-post-date">{widgetFor('date')}</p>
+          </span>
+          <div className="news-item-html">{widgetFor('body')}</div>
+          </li>
+      </ol>
   )
 }
-
-// NewsPostPreview.propTypes = {
-//   entry: PropTypes.shape({
-//     getIn: PropTypes.func,
-//   }),
-//   widgetFor: PropTypes.func,
-// }
 
 export default NewsPostPreview
