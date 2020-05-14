1f4603bc8163905bfab3e0ffea3ecc7413edd60b
Chris <sears.topher@gmail.com>
2020-05-14 13:37:42 -0400

    cms preview work

--- src/cms/preview-templates/NewsPostPreview.js
+++ src/cms/preview-templates/NewsPostPreview.js
@@ -1,18 +1,30 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
-import { NewsItem } from '../../components/newsItem'
+//import { NewsItem } from '../components/newsItem'
+
+import "../../styles/layout.css"
 
 const NewsPostPreview = ({ entry, widgetFor }) => {
-  const tags = entry.getIn(['data', 'tags'])
+//   const tags = entry.getIn(['data', 'tags'])
   return (
-    <NewsItem
-    //   content={widgetFor('body')}
-    //   description={entry.getIn(['data', 'description'])}
-    //   tags={tags && tags.toJS()}
-      html={widgetFor('body')}
-      title={entry.getIn(['data', 'title'])}
-      date={entry.getIn(['data', 'date'])}
-    />
+    <ol>
+        <li className="news-item">
+        <span className="news-item-header">
+            <h1 className="news-item-headline">{entry.getIn(['data', 'title'])}</h1>
+            <p className="news-post-date">{entry.getIn(['data', 'date'])}</p>
+        </span>
+        <div
+            className="news-item-html"
+            dangerouslySetInnerHTML={{ __html: widgetFor('body') }}
+        />
+        </li>
+    </ol>
+
+    // <NewsItem
+    //   html={widgetFor('body')}
+    //   title={entry.getIn(['data', 'title'])}
+    //   date={entry.getIn(['data', 'date'])}
+    // />
   )
 }
 
