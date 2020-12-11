c58f568a5fde701e754ee7f14a6df17250ff72f9
Chris <sears.topher@gmail.com>
2020-12-11 16:55:11 -0500

    sort courses asc

--- src/components/courses-grid.js
+++ src/components/courses-grid.js
@@ -7,7 +7,7 @@ const CoursesGrid = props => {
   const data = useStaticQuery(graphql`
     query {
       allMarkdownRemark(
-        sort: { fields: id, order: DESC }
+        sort: { fields: id, order: ASC }
         filter: { frontmatter: { templateKey: { eq: "course-page" } } }
       ) {
         edges {
